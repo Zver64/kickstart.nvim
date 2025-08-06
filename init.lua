082baed8a36f40d27ec4945117c42f47996212ed
Vasilii Chernov <vmchernov@inno.tech>
2025-08-06 17:54:07 +0300

    save

--- init.lua
+++ init.lua
@@ -430,9 +430,9 @@ if not vim.g.vscode then
           -- },
           -- },
           pickers = {
-            -- find_files = {
-            --   hidden = true,
-            -- },
+            find_files = {
+              hidden = true,
+            },
           },
           extensions = {
             ['ui-select'] = {
@@ -450,11 +450,12 @@ if not vim.g.vscode then
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>sG', builtin.git_status, { desc = 'Search changed files' })
         vim.keymap.set('n', '<leader>sa', function()
           builtin.find_files {
             no_ignore = true,
           }
-        end, { desc = '[S]earch [F]iles' })
+        end, { desc = '[S]earch All [F]iles' })
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -462,7 +463,6 @@ if not vim.g.vscode then
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-        vim.keymap.set('n', '<leader>sG', builtin.git_status, { desc = 'Search changed files' })
         vim.keymap.set('n', '<leader>sh', builtin.git_bcommits, { desc = 'Search file history' })
 
         -- Slightly advanced example of overriding default behavior and theme
